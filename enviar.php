e89c5b024dee2c1c52e10ec3a57154e45793bfb5
JBenAvil <148589379+JBenAvil@users.noreply.github.com>
2024-02-27 01:26:47 +0000

    c19

--- enviar.php
+++ enviar.php
@@ -1,4 +1,40 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    $destinatario = "juanbenavidesavila@email.com"; // Dirección de correo electrónico 
+    $asunto = "Mensaje de formulario web";
+    $contenido = "Nombre: $nombre\nCorreo electrónico: $email\nMensaje: $mensaje";
+
+    // Envía el correo electrónico
+    mail($destinatario, $asunto, $contenido);
+
+    // Redirige de vuelta a la página de origen o a una página de confirmación
+    header('Location: gracias.html');
+} else {
+    // Si alguien intenta acceder directamente al script, redirige a una página de error
+    header('Location: error.html');
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+/php 
 $destino = "biocenter.contactanos@gmail.com";
 $nombre = $_POST["nombre"];
 $apellido = $_POST["apellido"];
@@ -11,4 +47,5 @@ $contenido = "nombre:" . $nombre . "\napellido:" . $apellido . "\nrut:" . $rut .
 mail($destino, "consulta", $contenido);
 header("Location:index.html");
  
-?>
+
+-->
